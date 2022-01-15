d6c37a2259d8a4b70927c7dc559505fdc7067efc
Zin-Thu-Htet <zth.chung@gmail.com>
2022-01-15 17:10:42 +0630

    [zth]nav&sidebar finished

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,8 @@
 
   <!-- Styles -->
   <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+  <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+  <link rel="stylesheet" href="{{ asset('css/library/fontawesome.css') }}">
   @yield('css')
 
   <!-- Scripts -->
@@ -22,8 +24,48 @@
 
 <body>
 
-  @yield('content')
-  @yield('script')
+  <div class="wrapper d-flex">
+    <nav id="sidebar" class="active">
+      <h1><a href="#" class="logo">Employee Management</a></h1>
+      <ul class="components">
+        <li>
+          <a href="#"><span class="fas fa-tachometer-alt"></span> Dashboard</a>
+        </li>
+        <li>
+          <a href="#"><span class="fa fa-users"></span> Employee List</a>
+        </li>
+        <li>
+          <a href="#"><span class="fas fa-file-invoice"></span> Attendance</a>
+        </li>
+        <li>
+          <a href="#"><span class="fas fa-cash-register"></span> Payroll Management</a>
+        </li>
+      </ul>
+    </nav>
+    <!-- Page Content  -->
+    <div id="content">
+      <nav class="navbar">
+        <ul class="navbar-nav">
+          <li>
+            <img class="profile-img" src="#" id="img">
+          </li>
+          <li>
+            <div class="dropdown">
+              <button class="dropbtn btn">Username <i class="fas fa-caret-down"></i></button>
+              <div class="dropdown-content">
+                <a href="#">Profile</a>
+                <a href="#">Logout</a>
+              </div>
+            </div>
+          </li>
+        </ul>
+        <div>
+        </div>
+      </nav>
+      @yield('content')
+      @yield('script')
+    </div>
+  </div>
 </body>
 
 </html>
