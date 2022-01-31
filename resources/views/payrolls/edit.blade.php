bd71e882c1f9e700d7cbd459c1811a1b2daec627
Zin-Thu-Htet <zth.chung@gmail.com>
2022-01-31 17:15:44 +0630

    [feature/feature_ui_update_zth] Fixed reviews

--- resources/views/payrolls/edit.blade.php
+++ resources/views/payrolls/edit.blade.php
@@ -9,41 +9,43 @@
 @section('content')
 <div class="container">
   <div class="card">
-    <div class="listcard-header mr-table">
+    <div class="listcard-header">
       Edit Payroll
     </div>
-    <form action="{{ route('payrolls#updatePayroll', [$employee->id]) }}" method="POST">
-      {{ csrf_field() }}
-      @method('PATCH')
-      <div class="row clearfix">
-        <div class="float-left text">
-          <label for="basic_salary">Basic Salary</label>
+    <div class="card-body">
+      <form action="{{ route('payrolls#updatePayroll', [$employee->id]) }}" method="POST">
+        {{ csrf_field() }}
+        @method('PATCH')
+        <div class="row clearfix">
+          <div class="float-left text">
+            <label for="basic_salary">Basic Salary</label>
+          </div>
+          <div class="float-left input">
+            <input type="number" name="basic_salary" class="form-control" value="{{ $employee->salary->basic_salary }}" placeholder="MMK">
+          </div>
         </div>
-        <div class="float-left input">
-          <input type="number" name="basic_salary" class="form-control" value="{{ $employee->salary->basic_salary }}">
+        <div class="row clearfix">
+          <div class="float-left text">
+            <label for="overtime_fee">Overtime Fee</label>
+          </div>
+          <div class="float-left input">
+            <input type="number" name="overtime_fee" class="form-control" value="{{ $employee->salary->overtime_fee }}">
+          </div>
         </div>
-      </div>
-      <div class="row clearfix">
-        <div class="float-left text">
-          <label for="overtime_fee">Overtime Fee</label>
+        <div class="row clearfix">
+          <div class="float-left text">
+            <label for="leave_fine">Leave Fine</label>
+          </div>
+          <div class="float-left input">
+            <input type="number" name="leave_fine" class="form-control" value="{{ $employee->salary->leave_fine }}">
+          </div>
         </div>
-        <div class="float-left input">
-          <input type="number" name="overtime_fee" class="form-control" value="{{ $employee->salary->overtime_fee }}">
+        <div class="btn-group">
+          <input type="submit" value="Update" class="blue-btn">
+          <a href="#" id="back" class="red-btn">Cancel</a>
         </div>
-      </div>
-      <div class="row clearfix">
-        <div class="float-left text">
-          <label for="leave_fine">Leave Fine</label>
-        </div>
-        <div class="float-left input">
-          <input type="number" name="leave_fine" class="form-control" value="{{ $employee->salary->leave_fine }}">
-        </div>
-      </div>
-      <div class="btn-group">
-        <input type="submit" value="Update" class="blue-btn">
-        <a href="#" id="back" class="red-btn">Cancel</a>
-      </div>
-    </form>
+      </form>
+    </div>
   </div>
 </div>
 {!! JsValidator::formRequest('App\Http\Requests\UpdatePayrollRequest'); !!}
