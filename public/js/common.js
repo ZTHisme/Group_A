70eafea92b017d5d0b9867440cb6a8a2245219dc
Zin-Thu-Htet <zth.chung@gmail.com>
2022-02-01 18:07:13 +0630

    [feature/feature_responsive_nav&sidebar_zth] Finished responsive nav&sidebar

--- public/js/common.js
+++ public/js/common.js
@@ -21,4 +21,11 @@ $(function () {
     $('#back').on('click', function () {
         window.history.back();
     });
+    const body = document.querySelector('body'),
+        sidebar = body.querySelector('nav'),
+        toggle = body.querySelector(".toggle")
+
+    toggle.addEventListener("click", () => {
+        sidebar.classList.toggle("close");
+    })
 });
